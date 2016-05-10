3a3ebaa0b489c5f911239d6324db9049be277b0b
yangbin <2326176334@qq.com>
2016-05-10 08:45:27 +0800

    Initial Commit

---
+++ YBBackTransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBBackTransitionDemo
+//
+//  Created by YangBin-Mac on 16/5/10.
+//  Copyright © 2016年 YangBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YBBackTransitionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YBBackTransitionDemo
+//
+//  Created by YangBin-Mac on 16/5/10.
+//  Copyright © 2016年 YangBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
